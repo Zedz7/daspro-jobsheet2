31e1b15b33c9e275fe8e8f6a7aea84f685db7696
z1 <163568887+navz124@users.noreply.github.com>
2024-09-06 16:27:47 +0700

    Add files via upload

---
+++ Latihan06.java
@@ -0,0 +1,20 @@
+/**
+ * Latihan06
+ */
+public class Latihan06 {
+
+    public static void main(String[] args) {
+        System.out.print("\n---------------------------------------------------------");
+        System.out.print("\n-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.print("\n---------------------------------------------------------");
+        System.out.print("\nNama    : Hafiz Ahmad Zaim Kuntoro");
+        System.out.print("\nNIM     : 244107060103");
+        System.out.print("\nKelas   : SIB 1A");
+        System.out.print("\nNo Absen: 06");
+        System.out.print("\nProdi   : Sistem Informasi Bisnis");
+        System.out.print("\nAlamat  : Jalan Simpang Remujung NO 7 ");
+        System.out.print("\nNo HP   : 081285957920");
+        System.out.print("\nHobi    : Bermain Game");
+        System.out.println("HALOOO");
+    }
+}
